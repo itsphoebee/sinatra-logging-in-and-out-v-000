fcee3e9396210fc27bb0a8c77a17aff3e5f3db0b
Phoebe Lau <phoebelau7@gmail.com>
2018-01-12 22:46:41 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    if Helpers.is_logged_in?
+    if session[:user_id] != nil
       erb :account
     else
       erb :error
