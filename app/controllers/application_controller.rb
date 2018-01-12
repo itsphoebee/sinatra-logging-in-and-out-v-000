4da90eb734d2fec6cdf53c7a1feeb345c6ed7876
Phoebe Lau <phoebelau7@gmail.com>
2018-01-12 22:13:46 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    if User.find_by(username:params[:username])
+    @user = User.find_by(username:params[:username])
+    if @user
       session[:user_id] = user.id
       redirect '/account'
     else
